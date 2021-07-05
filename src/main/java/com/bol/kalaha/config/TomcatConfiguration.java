e935509bc2c4f40405857b9867fc9ecfbcd840ea
altayhunoglu <hunoglualtay@gmail.com>
2021-07-05 19:37:48 +0300

    allowed origins2

---
+++ src/main/java/com/bol/kalaha/config/TomcatConfiguration.java
@@ -0,0 +1,39 @@
+package com.bol.kalaha.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.websocket.server.WsSci;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfiguration {
+    @Value("${PORT:8080}")
+    int port;
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createSslConnector());
+        return tomcat;
+    }
+
+    private Connector createSslConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(port);
+        connector.setSecure(false);
+        // Monitor 8080 port forward to 443 port
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    @Bean
+    public TomcatContextCustomizer tomcatContextCustomizer() {
+        return context -> context.addServletContainerInitializer(new WsSci(), null);
+    }
+
+}
